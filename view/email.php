12d89422d2442c5b29947852c85f73c9c90fac76
Nathan-Silva <braganathan38@gmail.com>
2020-06-25 04:15:23 -0300

    Função email iniciado 25.06

---
+++ view/email.php
@@ -0,0 +1,39 @@
+<?php
+require_once('../src/PHPMailer.php');
+require_once('../src/SMTP.php');
+require_once('../src/Exception.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+$nome = $_POST['Nome'];
+$email = $_POST['Email'];
+
+try{
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'braganathan38@gmail.com';
+    $mail->Password = '@270100@';
+    $mail->Port = 587;
+
+    $mail->setFrom('braganathan38@gmail.com');
+    $mail->addAddress('braganathan38@gmail.com');
+    
+    $mail->isHTML(true);
+    $mail->Subject = '*** NOVO CADASTRO LEAD ***';
+    $mail->Body = "Novo cliente cadastrado via formulário: <br><br> Nome: <strong>". $nome . "<br>Email: " . $email . "<br><br><br> *** FINAL DE AVISO ***";
+    $mail->AltBody = 'Chegoooou';
+
+    if($mail->send()){
+        echo "<script>alert('teste');</script>";
+    }else{
+        echo "Email nao enviado";
+    }
+
+}catch(Exception $e){
+    echo "Erro ao encaminhar email: {$mail->ErrorInfo}";
+}
